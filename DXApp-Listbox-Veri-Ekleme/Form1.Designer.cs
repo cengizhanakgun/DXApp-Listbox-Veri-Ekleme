07cfe5dc4958d339dc58c6d57ead63a32e8bdc6a
Cengizhan Akgün <coderista@gmail.com>
2024-02-28 23:36:07 +0300

    Add project files.

---
+++ DXApp-Listbox-Veri-Ekleme/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace DXApp_Listbox_Veri_Ekleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(12, 52);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(198, 23);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Ekle";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(12, 21);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(198, 20);
+            this.textEdit1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(198, 329);
+            this.listBox1.TabIndex = 2;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(12, 2);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(68, 13);
+            this.labelControl1.TabIndex = 3;
+            this.labelControl1.Text = "Eklenecek Veri";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(12, 81);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(70, 13);
+            this.labelControl2.TabIndex = 4;
+            this.labelControl2.Text = "Eklenen Veriler";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(12, 435);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(145, 13);
+            this.labelControl3.TabIndex = 5;
+            this.labelControl3.Text = "cengizhanakgun.blogspot.com";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 461);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "Form1";
+            this.Text = "cengizhanakgun.blogspot.com";
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private System.Windows.Forms.ListBox listBox1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+    }
+}
+
---
+++ DXApp-Listbox-Veri-Ekleme/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DXApp_Listbox_Veri_Ekleme
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        int i = 0;
+        string[] dizi = new string[10];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (i < 10)
+            {
+                // Kullanıcı tarafından girilen metin
+                string newItem = textEdit1.Text;
+                // İlk harfi büyük yap
+                newItem = char.ToUpper(newItem[0]) + newItem.Substring(1);
+
+                // Diziye ve ListBox'a öğe ekle
+                dizi[i] = newItem;
+                listBox1.Items.Add(newItem);
+                i++;
+                textEdit1.Clear();
+                textEdit1.Focus();
+            }
+            
+
+        }
+    }
+}
